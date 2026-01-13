c93d884be00ed34baa268b8f238726301fe70175
Markian-Grybok <grybokmarkian@gmail.com>
2026-01-13 14:53:11 +0200

    Add extension

---
+++ backend/EducationContentService/EducationContentService.Core/Configuration/ServiceCollectionExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace EducationContentService.Core.Configuration;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddCoreConfiguration(this IServiceCollection services)
+    {
+        // Add core configuration services here
+        return services;
+    }
+}
