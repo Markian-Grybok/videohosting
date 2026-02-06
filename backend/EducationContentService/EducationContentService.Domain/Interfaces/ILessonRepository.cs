a2a9d8d144a1dc8374593d76c57db685ff574a3b
Markian-Grybok <grybokmarkian@gmail.com>
2026-02-06 23:55:44 +0200

    add interfaces

---
+++ backend/EducationContentService/EducationContentService.Domain/Interfaces/ILessonRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace EducationContentService.Domain.Interfaces;
+
+public interface ILessonRepository
+{
+        Task AddAsync(Lesson.Lesson lesson, CancellationToken cancellationToken);
+        Task<Lesson.Lesson?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+        Task UpdateAsync(Lesson.Lesson lesson, CancellationToken cancellationToken);
+        Task DeleteAsync(Lesson.Lesson lesson, CancellationToken cancellationToken);
+}
